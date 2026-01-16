d1c2b0e0b2c488959877af2850348130b219a8cf
Jorge Flores <jorgefloresramirez01@gmail.com>
2026-01-16 02:17:12 -0800

    Add mathFuncations.c tutorial with examples

---
+++ tutorial/mathFuncations.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+// math functions = perform mathematical operations
+// below are comments with examples of functions
+// feel free to uncomment them when you want to see what they do
+// top is int and bottom is floats
+int main(){
+    
+    // int x = -3;
+    // x = sqrt(x);
+    // x = pow(x, 2);
+    // x = abs(x);
+
+    float x = 45;
+    // x = round(x);
+    // x = ceil(x);
+    // x = floor(x);
+    // x = log(x);
+    // x = sin(x);
+    // x = cos(x);
+    x = tan(x);
+
+
+    printf("%f\n",x);
+    return 0;
+}
